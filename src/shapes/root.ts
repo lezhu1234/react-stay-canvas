16d8895577e4d9432f3eb8e324d7da404ff79eff
lezhu1234 <1625751399@qq.com>
2024-07-15 18:52:57 +0800

    fix root container bug

--- src/shapes/root.ts
+++ src/shapes/root.ts
@@ -1,8 +1,29 @@
-import { Rectangle } from "./rectangle"
+import { Point } from "./point"
+import { Rectangle, RectangleAttr } from "./rectangle"
 
 export class Root extends Rectangle {
+  initX: number
+  initY: number
+  initWidth: number
+  initHeight: number
+  constructor({ x, y, width, height, props = {} }: RectangleAttr) {
+    super({ x, y, width, height, props })
+    this.initX = x
+    this.initY = y
+    this.initWidth = width
+    this.initHeight = height
+  }
   copy() {
     return this
   }
   draw() {}
+
+  contains(point: Point): boolean {
+    return (
+      point.x > this.initX &&
+      point.x < this.initX + this.initWidth &&
+      point.y > this.initY &&
+      point.y < this.initY + this.initHeight
+    )
+  }
 }
